840e44f8e7b88cb9dd83d3dd544fb272845444c2
Juan Jose <juanjo280701@gmail.com>
2025-12-08 11:36:05 +0100

    changed one file path in file_conversion.R

--- file_conversion.R
+++ file_conversion.R
@@ -10,7 +10,7 @@ invisible(lapply(packages, library, character.only = TRUE))
 # df_numeric <- zap_labels(df)
 # write_csv(df_numeric, './data/Environmental_Attitudes.csv')
 
-file_path <- "./data/NUTS_RG_20M_2021_3035.shp/NUTS_RG_20M_2021_3035.shp"
+file_path <- "./data/NUTS/NUTS_RG_20M_2021_3035.shp"
 map_data <- st_read(file_path)
 print(map_data)
 head(map_data)
