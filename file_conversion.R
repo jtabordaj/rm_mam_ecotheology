e230617b4e7a6e0e004a3c07f042a67d64dd0d26
Juan Jose <juanjo280701@gmail.com>
2025-12-08 11:06:47 +0100

    added sf package and standardized filenames

--- file_conversion.R
+++ file_conversion.R
@@ -1,7 +1,15 @@
 library(haven)
 library(writexl)
 library(readr)
-df <- read_sav("./data/Environmental_attitudes.sav")
-df_numeric <- zap_labels(df)
+library(sf)
 
-write_csv(df_numeric, './data/Environmental_Attitudes.csv')
+# df <- read_sav("./data/Environmental_attitudes.sav")
+# df_numeric <- zap_labels(df)
+# write_csv(df_numeric, './data/Environmental_Attitudes.csv')
+
+file_path <- "./data/NUTS_RG_20M_2021_3035.shp/NUTS_RG_20M_2021_3035.shp"
+map_data <- st_read(file_path)
+print(map_data)
+head(map_data)
+names(map_data)
+plot(st_geometry(map_data))
