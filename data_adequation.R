6ef9b33a270525c656f370f0db2152f3cdfc2f3d
Juan Jose <juanjo280701@gmail.com>
2025-12-09 16:12:58 +0100

    Matched dataEnvironmental with mapEurope

--- data_adequation.R
+++ data_adequation.R
@@ -15,7 +15,7 @@ mapEurope <- st_transform(mapEurope, 4326)
 mapEurope <- st_crop(mapEurope, xmin = -10, xmax = 45, ymin = 0, ymax = 69)
 mapEurope <- st_transform(mapEurope, 3035)
 mapEurope <- st_make_valid(mapEurope)
-plot(st_geometry(mapEurope))
+# plot(st_geometry(mapEurope))
 
 ## 2. To enrich NUTS data with Monastery location
 # First, Franciscan and Dominican latitudes and longitudes become a geometrical object through the following lines
@@ -30,3 +30,15 @@ dataDominican <- st_transform(dataDominican, 3035)
 
 enrich_monastery_data(dataFranciscan)
 enrich_monastery_data(dataDominican)
+
+## 3. To enrich NUTS data with environmental attitudes
+# We select the columns relevant to our project and rename v275b_N2, which corresponds to NUTS 2 ID, to NUTS_ID as in mapEurope
+
+dataEnvironmental <- dataEnvironmental %>% select(
+    studyno, doi, studynoc, id_cocas, caseno, year, country, c_abrv, cntry_y, 
+    v13, v129, v199, v200, v201, v202, v203, v204,
+    v275b_N2, v275c_N2
+)
+names(dataEnvironmental)[names(dataEnvironmental) == "v275b_N2"] <- "NUTS_ID"
+
+dataEnvironmental <- left_join(dataEnvironmental, mapEurope, by = "NUTS_ID")
