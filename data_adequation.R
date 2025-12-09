cf0a2c12580003f4217bacfb06b9d1b5626cb6a9
Juan Jose <juanjo280701@gmail.com>
2025-12-09 19:27:01 +0100

    standardized to crs 4326 and added HYDE compatibility

--- data_adequation.R
+++ data_adequation.R
@@ -13,20 +13,10 @@ mapEurope <- mapEurope %>% filter(LEVL_CODE %in% c(2))
 
 mapEurope <- st_transform(mapEurope, 4326)
 mapEurope <- st_crop(mapEurope, xmin = -10, xmax = 45, ymin = 0, ymax = 69)
-mapEurope <- st_transform(mapEurope, 3035)
 mapEurope <- st_make_valid(mapEurope)
-# plot(st_geometry(mapEurope))
+plot(st_geometry(mapEurope))
 
 ## 2. To enrich NUTS data with Monastery location
-# First, Franciscan and Dominican latitudes and longitudes become a geometrical object through the following lines
-
-dataFranciscan <- dataFranciscan %>% st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE)
-dataDominican <- dataDominican %>% st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE)
-
-dataFranciscan <- st_transform(dataFranciscan, 3035)
-dataDominican <- st_transform(dataDominican, 3035)
-
-# Now that everything is standardized we join monastery data with the NUTS dataset at level 2
 
 enrich_monastery_data(dataFranciscan)
 enrich_monastery_data(dataDominican)
--- dependencies.R
+++ dependencies.R
@@ -1,25 +1,47 @@
 Sys.setlocale('LC_ALL','en_US.UTF-8')
-packages <- c("haven", "writexl", "sf", "readr", "readxl", "dplyr", "ggplot2", "stringr")
+options(timeout = 600)
+options(scipen = 999)
+packages <- c("haven", "writexl", "sf", "readr", "readxl", "dplyr", "ggplot2", "stringr", "terra")
 packages_to_install <- packages[!(packages %in% installed.packages()[,"Package"])]
 if (length(packages_to_install) > 0) {
   install.packages(packages_to_install, dependencies = TRUE)
 }
 invisible(lapply(packages, library, character.only = TRUE))
-file_path_nuts <- "./data/NUTS.shp/NUTS_RG_20M_2021_3035.shp"
 
-mapEurope <- st_read(file_path_nuts)
-dataEnvironmental <- read_csv("./data/environmental/environmental_attitudes.csv")
-dataDominican <- read_csv("./data/houses/mps_dominican_1216_1500.csv")
-dataFranciscan <- read_csv("./data/houses/mps_franciscan_1300.csv")
+# Path
+online_path_hyde <- "https://geo.public.data.uu.nl/vault-hyde/HYDE%203.3%5B1710493486%5D/original/hyde33_c7_base_mrt2023/NetCDF/population.nc"
+local_path_hyde <- "./data/hyde/hyde_grid.nc"
+local_path_nuts <- "./data/NUTS.shp/NUTS_RG_20M_2021_3035.shp"
+local_path_environmental <- "./data/environmental/environmental_attitudes.csv"
+local_path_dominican <- "./data/houses/mps_dominican_1216_1500.csv"
+local_path_franciscan <- "./data/houses/mps_franciscan_1300.csv"
 
-# df <- read_sav("./data/Environmental_attitudes.sav")
-# df_numeric <- zap_labels(df)
-# write_csv(df_numeric, './data/Environmental_Attitudes.csv')
+# Read
+mapEurope <- st_read(local_path_nuts)
+dataEnvironmental <- read_csv(local_path_environmental, show_col_types = FALSE)
+dataDominican <- read_csv(local_path_dominican, show_col_types = FALSE)
+dataFranciscan <- read_csv(local_path_franciscan, show_col_types = FALSE)
 
+if (file.exists(local_path_hyde)) {
+  message("Loading HYDE...")
+  hyde_data <- rast(local_path_hyde)
+  message("...SUCCESS")
+} else {
+  message("Downloading HYDE...")
+  tryCatch({
+    download.file(url = online_path_hyde, destfile = "./data/hyde/hyde_grid.nc", mode = "wb")
+    hyde_data <- rast(local_path_hyde)
+    message("...SUCCESS")
+  }, error = function(e) {
+    message("...FAIL")
+    print(e)
+  })
+}
+
+# Functions
 enrich_monastery_data <- function(dataset){
   target <- dataset
   target <- target %>% st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE)
-  target <- st_transform(target, 3035)
   join <- st_join(target, mapEurope, join = st_intersects)
   join <- coerceNAPoints(join)
   assign(paste(
