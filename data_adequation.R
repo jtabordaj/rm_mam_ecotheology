88d2e2a0fa3b2a7c42fbf5e781b97392df406c8a
Juan Jose <juanjo280701@gmail.com>
2025-12-11 13:07:28 +0100

    fixed a crs compatibility error

--- data_adequation.R
+++ data_adequation.R
@@ -3,7 +3,6 @@ source('./dependencies.R')
 ## 1. To clean Europe's map
 # We remove islands and overseas territories, such as Iceland, French peripherals, Azores, Madeira, Canary Islands.
 # NUTS 2 specificity
-
 overseasTerritories <- c("^FRY", "^PT2", "^PT3", "^ES7", "^IS") # Umbrella term for anything not close to mainland Europe + Britain
 filterStatement <- paste(overseasTerritories, collapse = "|")
 mapEurope <- mapEurope %>% filter(!str_detect(NUTS_ID, filterStatement))
@@ -11,10 +10,10 @@ mapEurope <- mapEurope %>% filter(LEVL_CODE %in% c(2))
 
 # We also zoom-in the map in Europe, latitudinal/longitudinal coordinates are handcrafted based on the project data
 
-mapEurope <- st_transform(mapEurope, 4326)
+mapEurope <- st_transform(mapEurope, crs(dataHYDE))
 mapEurope <- st_crop(mapEurope, xmin = -10, xmax = 45, ymin = 0, ymax = 69) 
 mapEurope <- st_make_valid(mapEurope)
-# plot(st_geometry(mapEurope))
+plot(st_geometry(mapEurope))
 
 # Prompt used in Gemini 3.0 for this section: Give me the furthest cardinal points of the European continent, constrained to NUTS 2 regions.
 # We then adjusted +-1 or 2 degrees based on existing data
@@ -37,6 +36,7 @@ dataEnvironmental <- left_join(dataEnvironmental, mapEurope, by = "NUTS_ID")
 
 ## 4. Working with HYDE data (3.3 Version, Baseline)
 
+# dataHYDE <- project(dataHYDE, 'epsg:4326')
 print(names(dataHYDE))
 dataHYDE <- dataHYDE[[21:28]] # population_21 = 1000 CE, scales century-wise until population_28 = 1700
 names(dataHYDE) <- c("pop_1000", "pop_1100", "pop_1200", "pop_1300", "pop_1400", "pop_1500", "pop_1600", "pop_1700")
