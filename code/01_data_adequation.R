a86f64b59244b7e573965e6788c39bfd859bdb65
gpantalena <giovanna.pantalena02@gmail.com>
2026-01-15 17:36:58 +0100

    Adding control variables and cleaning them

--- code/01_data_adequation.R
+++ code/01_data_adequation.R
@@ -31,8 +31,18 @@ map_monasteries(dataDominican)
 ## 3. To enrich NUTS data with environmental attitudes
 dataEnvironmental <- dataEnvironmental %>% select(
     studyno, doi, studynoc, id_cocas, caseno, year, country, c_abrv, cntry_y, 
-    v129, v199, v200, v201, v202, v203, v204, v275b_N1, v275b_N2
+    v129, v199, v200, v201, v202, v203, v204, # Environmental Variables
+    v275b_N1, v275b_N2, # NUTS Variables
+    v225,           # Gender
+    age,            # Age (constructed)
+    v276_r,         # Town Size
+    v243_edulvlb,   # Education
+    v261_ppp,       # Income PPP
+    v246_ISEI,      # Socio-Economic Status
+    v174_LR,        # Political Scale
+    v52             # Religion
 ) 
+
 dataEnvironmental <- dataEnvironmental %>% 
     mutate(
         v275b_N2 = ifelse(v275b_N2 == "-4", v275b_N1, v275b_N2)
@@ -41,24 +51,44 @@ dataEnvironmental <- dataEnvironmental %>%
 names(dataEnvironmental)[names(dataEnvironmental) == "v275b_N2"] <- "NUTS_ID"
 
 dataEnvironmental <- dataEnvironmental %>% 
-    mutate( # Mutate Idea: Values further away from 1 indicate less pro-environmental attitudes
+    mutate( 
+      # --- Environmental Index Vars ---
+      # Mutate Idea: Values further away from 1 indicate less pro-environmental attitudes
         envir_orgs_confidence =  ifelse(v129 %in% c(1, 2, 3, 4), v129, NA), # v129: How much confidence you have in environmental orgs.
         envir_econ_priority = ifelse(v204 %in% c(1, 2), v204, NA), # v204: Growth vs Protection priorities.
         envir_protection_money = ifelse(v199 > 0, v199, NA), # v199: Give income to environmental causes.
         envir_efforts_pointless = ifelse(v200 %in% c(1, 2, 3 , 4 , 5), 6 - v200, NA), # v200: Too difficult for someone like me to do much about the environment. REVERSED DIRECTION
         envir_other_importances = ifelse(v201 %in% c(1, 2, 3 , 4, 5), 6 - v201, NA), # v201: There are more important things to do in life than protect the environment
         envir_network_effect = ifelse(v202 %in% c(1, 2, 3, 4, 5), 6 - v202, NA), # v202: No point in doing what I can for the environment unless others do the same
-        envir_threats_exaggerated = ifelse(v203 %in% c(1, 2, 3, 4, 5), 6 - v203, NA) # v203: Many of the claims about environmental threats are exaggerated.
+        envir_threats_exaggerated = ifelse(v203 %in% c(1, 2, 3, 4, 5), 6 - v203, NA), # v203: Many of the claims about environmental threats are exaggerated.
+      # --- Control Variables ---
+        gender_female = ifelse(v225 == 2, 1, 0),
+        age_clean = ifelse(age > 0, age, NA),
+        town_size = ifelse(v276_r > 0, v276_r, NA),
+        education = ifelse(v243_edulvlb >= 0, v243_edulvlb, NA),
+        income_ppp = ifelse(v261_ppp >= 0, v261_ppp, NA),
+        sei_status = ifelse(v246_ISEI > 0, v246_ISEI, NA),
+        political_right = ifelse(v174_LR >= 1, v174_LR, NA),
+        is_catholic = ifelse(v52 == 1, 1, 0),
+        is_protestant = ifelse(v52 == 2, 1, 0)
     ) %>% 
     na.omit()
-#
+## Note: na.omit() removes rows where ANY variable is NA. 
+    # Income often has many NAs, so your sample size will drop. 
+    # If the drop is too large, remove 'income_ppp' from the select below.
 
-dataEnvironmental <- left_join(dataEnvironmental, mapEurope, by = "NUTS_ID")
+dataEnvironmental <- left_join(
+    dataEnvironmental, 
+    mapEurope %>% distinct(NUTS_ID, .keep_all = TRUE), 
+    by = "NUTS_ID"
+)
 
 dataEnvironmental <- dataEnvironmental %>% select(
-    country, c_abrv, cntry_y, NUTS_ID, 
+    studyno, caseno, country, c_abrv, cntry_y, NUTS_ID,
     envir_econ_priority, envir_efforts_pointless, envir_other_importances, 
     envir_network_effect, envir_threats_exaggerated, envir_protection_money,
+    gender_female, age_clean, town_size, education, income_ppp, sei_status, 
+    political_right, is_catholic, is_protestant,
     LEVL_CODE, NAME_LATN, NUTS_NAME, geometry
     )
 #
@@ -122,11 +152,11 @@ dataEnvironmental <- dataEnvironmental %>%
   mutate(across(all_of(exposure_cols), ~replace_na(., 0)))
 
 # E. Final Sanity Check
-# We check if we accidentally created duplicates during the join
-if(any(duplicated(dataEnvironmental$studyno))) {
+# We check 'id_cocas' which is the unique global ID for respondents.
+if(any(duplicated(dataEnvironmental$id_cocas))) {
   warning("Duplicated respondents detected! Check the join logic.")
 } else {
-  message("Data Adequation Complete")
+  message("Data Adequation Complete. 'dataEnvironmental' is clean and ready.")
 }
 
 
